adadc89dde9e0dc39fb210e7c8366277dc4e91f6
Lorg <hikariame100@gmail.com>
2022-09-02 15:11:23 -0400

    Initial commit

---
+++ Warmup/Assets/Scripts/CircleSpawn.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleSpawn : MonoBehaviour
+{
+    public Sprite[] sprites;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate(Resources.Load("Prefabs/Circle"));
+        InvokeRepeating("Spawn", 1,1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        GameObject tgo = Instantiate(Resources.Load("Prefabs/Circle")) as GameObject;
+        int num = GetComponent<ColorPicker>().SetSprite();
+        tgo.GetComponent<SpriteRenderer>().sprite = sprites[num];
+    }
+}
